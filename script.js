ee42ea9a5a3decd1758e3ef5903b7a636a41a8b1
Vitor Dilucca <vitordilucca@hotmail.com>
2023-02-17 15:51:10 -0300

    hasblackjack? Boolean

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 let firstCard = 10
-let secondCard = 19
+let secondCard = 11
+let hasBlackJack = false
 
 let sum = firstCard+secondCard
 console.log(sum)
 
-if (sum < 21) {
-  console.log("do you want to draw a new card?")
+if (sum<=20) {
+  console.log("quer outra carta?")
 }else if(sum===21){
-  console.log("congratulations voce venceu! you have blackjack")
-}else {
-  console.log("you lost.")
-
+  console.log("you've got blackjack")
+  hasBlackJack=true
+}else{
+  console.log("you are out of the game!")
 }
 
+//cash out!
+console.log(hasBlackJack)
 // exercicio
